ed19398ec7035c52d6276ce7feca4b8efe6c94f8
Zohid-Jurakulovich <zohidjurakulovich@gmail.com>
2022-02-23 21:22:11 +0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var elForm = document.querySelector(".form");
+var elInput = document.querySelector(".form-control");
+var elSelect = document.querySelector(".form-select");
+var elResult = document.querySelector(".result")
+
+var elUzs = document.querySelector(".uzs");
+var elRubl = document.querySelector(".rubl");
+var elEuro = document.querySelector(".euro")
+
+console.log(elForm)
+
+
+elForm.addEventListener("submit", function(event){
+event.preventDefault();
+
+// var elUzsValue = Number(elUzs.value);
+// var elRublValue = Number(elRubl.value);
+// var elEuroVal = Number(elEuro.value);
+
+var elInputVal = Number(elInput.value)
+var elSelectVal = Number(elSelect.value)
+
+var allResult = Number(elInputVal * elSelectVal);
+
+elResult.textContent = (`${allResult} `)
+})
